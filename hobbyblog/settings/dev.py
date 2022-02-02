f24493090beabb2cf70b55b51e22012188f00474
Gene Higgins <genotype@gknow.com>
2022-02-02 10:41:40 -0700

    first commit

---
+++ hobbyblog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-8*ry4^)we#5%gz39!3iw6n#cb(jx4d%y6ypfar9+k@4r*s17uz'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
